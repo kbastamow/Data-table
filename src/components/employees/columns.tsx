7dd6f2429be4e13975f2aafa1ec2434029965911
kbastamow <bastamowkat@gmail.com>
2023-09-13 11:08:26 +0200

    Sorting, filtering, column hide, row select options

--- src/components/employees/columns.tsx
+++ src/components/employees/columns.tsx
@@ -1,8 +1,11 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { Employee } from "../../types/employee";
+import { HiArrowsUpDown } from "react-icons/hi2";
+import { RiMore2Fill } from "react-icons/ri";
 // import { MoreHorizontal } from "lucide-react"
  
 import { Button } from "@/components/ui/button"
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,16 +14,42 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Checkbox } from "../ui/checkbox";
 
 
 export const columns: ColumnDef<Employee>[] = [
   {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
     accessorKey: "name",
-    header: () => <div className="text-right">Name</div>,
+    header: ({ column }) => {
+      return   (
+        <div className="flex justify-between text-left display ">Name
+            <HiArrowsUpDown className="w-4 h-4 ml-2 cursor-pointer" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}/>
+        </div>
+      )
+    },
     cell: ({row}) => {
         const { name } = row.original;
-        return <div className="text-right font-medium">{name}</div>
-    }
+        return <div className="font-medium text-left ">{name}</div>
+    },
   },
   {
     accessorKey: "email",
@@ -46,9 +75,8 @@ export const columns: ColumnDef<Employee>[] = [
       return (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              {/* <MoreHorizontal className="h-4 w-4" /> */}
+            <Button variant="ghost" className="w-8 h-8 p-0">
+           <RiMore2Fill></RiMore2Fill>
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
@@ -56,11 +84,11 @@ export const columns: ColumnDef<Employee>[] = [
             <DropdownMenuItem
               onClick={() => navigator.clipboard.writeText(employee.id)}
             >
-              Copy payment ID
+              Copy employee id
             </DropdownMenuItem>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
+            <DropdownMenuItem>View employee</DropdownMenuItem>
+            <DropdownMenuItem>View more details</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       )
--- src/components/employees/data-table.tsx
+++ src/components/employees/data-table.tsx
@@ -1,43 +1,128 @@
+import { useState } from "react";
+
 import {
-    ColumnDef,
-    flexRender,
-    getCoreRowModel,
-    useReactTable,
-    getPaginationRowModel,
-  } from "@tanstack/react-table"
-   
-  import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow
-  } from "@/components/ui/table"
-import { Button } from "../ui/button"
-   
-  interface DataTableProps<TData, TValue> {
-    columns: ColumnDef<TData, TValue>[]
-    data: TData[]
-  }
-   
-  export function DataTable<TData, TValue>({
-    columns,
+  ColumnDef,
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+  VisibilityState,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  //STATES:
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
+    [],
+  );
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+
+  const table = useReactTable({
     data,
-  }: DataTableProps<TData, TValue>) {
-    const table = useReactTable({
-      data,
-      columns,
-      getCoreRowModel: getCoreRowModel(),
-      getPaginationRowModel: getPaginationRowModel(),
-      initialState: {
-        pagination: { pageSize: 5 },
-      },
-    })
-   
-    return (
-        <div>
-      <div className="rounded-md border">
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    //sorting:
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+    },
+    //pagination:
+    getPaginationRowModel: getPaginationRowModel(),
+
+    //filters
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+
+    //Visibility:
+    onColumnVisibilityChange: setColumnVisibility,
+
+    //Control pagination. Default is 10
+    initialState: {
+      pagination: { pageSize: 5 },
+    },
+  });
+
+  return (
+    <div>
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter by name"
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("name")?.setFilterValue(event.target.value)}
+          className="max-w-sm"
+        />
+      </div>
+      {/* Column selection button */}
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="ml-auto">
+            Columns
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          {table
+            .getAllColumns()
+            .filter(
+              (column) => column.getCanHide(),
+            )
+            .map((column) => {
+              return (
+                <DropdownMenuCheckboxItem
+                  key={column.id}
+                  className="capitalize"
+                  checked={column.getIsVisible()}
+                  onCheckedChange={(value) => column.toggleVisibility(!!value)}
+                >
+                  {column.id}
+                </DropdownMenuCheckboxItem>
+              );
+            })}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      
+      {/* Information about selected rows */}
+      <div className="flex-1 text-sm text-right text-muted-foreground">
+        {table.getFilteredSelectedRowModel().rows.length} of{" "}
+        {table.getFilteredRowModel().rows.length} row(s) selected.
+      </div>
+
+      <div className="border rounded-md">
         <Table>
           <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
@@ -45,44 +130,49 @@ import { Button } from "../ui/button"
                 {headerGroup.headers.map((header) => {
                   return (
                     <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
+                      {header.isPlaceholder ? null : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
                     </TableHead>
-                  )
+                  );
                 })}
               </TableRow>
             ))}
           </TableHeader>
           <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                    </TableCell>
-                  ))}
+            {table.getRowModel().rows?.length
+              ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              )
+              : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={columns.length} className="h-24 text-center">
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
+              )}
           </TableBody>
         </Table>
-        
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
+      <div className="flex items-center justify-end py-4 space-x-2">
         <Button
           variant="outline"
           size="sm"
@@ -100,6 +190,6 @@ import { Button } from "../ui/button"
           Next
         </Button>
       </div>
-      </div>
-    )
-  }
+    </div>
+  );
+}

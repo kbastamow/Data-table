da33f0f97926123290f7f2118d4d1fd8b65bfebc
kbastamow <bastamowkat@gmail.com>
2023-09-14 11:30:57 +0200

    More controllable pagination

--- src/components/reusable/pagination-controls.tsx
+++ src/components/reusable/pagination-controls.tsx
@@ -10,14 +10,6 @@ import { Table } from "@tanstack/react-table";
 
 import { Button } from "../ui/button";
 
-//   import {
-//     Select,
-//     SelectContent,
-//     SelectItem,
-//     SelectTrigger,
-//     SelectValue,
-//   } from "@/registry/new-york/ui/select"
-
 interface DataTablePaginationProps<TData> {
   table: Table<TData>;
 }
@@ -54,23 +46,6 @@ export function DataTablePagination<TData>({
             </FiChevronDown>
           </div>
           {
-            /* <Select
-              value={`${table.getState().pagination.pageSize}`}
-              onValueChange={(value) => {
-                table.setPageSize(Number(value))
-              }}
-            >
-              <SelectTrigger className="h-8 w-[70px]">
-                <SelectValue placeholder={table.getState().pagination.pageSize} />
-              </SelectTrigger>
-              <SelectContent side="top">
-                {[10, 20, 30, 40, 50].map((pageSize) => (
-                  <SelectItem key={pageSize} value={`${pageSize}`}>
-                    {pageSize}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select> */
           }
         </div>
         <div className="flex w-[100px] items-center justify-center text-sm font-medium">
